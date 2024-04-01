c40297a41b2cbfe19bb3190cb09ae03c3b28ef47
fibrahim30 <fibrahim30@gatech.edu>
2024-04-01 17:40:46 -0400

    Added scripts to generate i5O dataset index files in csv format

---
+++ src/datasets/utils/i5Ocsv_export_undercoverright.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This script is to run the program one video at a time automatically (video paths variable takes a list of video path inputs), and ensure that each folder has only one videos.
+#
+
+for ARGUMENT in "$@"; do
+	KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+	KEY_LENGTH=${#KEY}
+	VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+	export "$KEY"="$VALUE"
+done
+
+# Specify the root directory
+videos_dir="/data/i5O/i5OData/undercover-right/videos/"
+echo $videos_dir
+
+# Specify the existing CSV file to append to
+existing_csv="i5O_index_file.csv"
+
+# Check if the directory exists
+if [ -d "$videos_dir" ]; then
+    # Find directories and their files
+    for folder in $(find "$videos_dir" -mindepth 1 -maxdepth 1 -type d); do
+        for file in $(find "$folder" -maxdepth 1 -type f -name "*.mp4"); do
+            file="${file/videos\//videos}"
+            # Append absolute file path to existing CSV file
+            echo "$file" >> "$existing_csv"
+        done
+    done
+    echo "Data appended to $existing_csv"
+else
+    echo "Directory $videos_dir does not exist."
+fi
